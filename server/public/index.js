8a1dc427c4d3b3fabc0d32e6589d650168a254b0
Ashish Shevale <shevaleashish@gmail.com>
2022-01-03 19:26:17 +0530

    Some helpful comments

--- server/public/index.js
+++ server/public/index.js
@@ -35,29 +35,35 @@ taskSubmitButton.addEventListener("click", (event) => {
   })
 })
 
-
+// Method for making CRUD request using the fetch api.
 async function makeRequest(url, method, body) {
-  console.log(JSON.stringify(body));
+  // Create the whole url.
   url = URL + url;
 
+  // This data defines the request we make.
   let init = {
-    method: method,
+    method: method, // GET, POST, UPDATE, DELETE
     mode: "cors",
     headers: {
-      "Content-Type": 'application/json',
+      "Content-Type": 'application/json', // type of data we are sending.
     }
   }
 
+  // For GET method, we must not provide the body attribute.
+  // that's why we are adding the property seperately.
   if (method != "GET") {
     init.body = JSON.stringify(body);
   }
 
+  // Get the response.
   const response = await fetch(url, init);
+  // Convert the response the json.
   const data = await response.json();
 
   return data;
 }
 
+// Just creates the HTML for the new task.
 function createNewTask(name) {
   return `<div class="task">
       <h5>${name}</h5 >
@@ -68,6 +74,7 @@ function createNewTask(name) {
     </div >`
 }
 
+// When the DOM loads, make a get request and fetch all the tasks.
 document.addEventListener("DOMContentLoaded", () => {
   makeRequest("/tasks", "GET", null).then((data) => {
     let innerHTML = "";
